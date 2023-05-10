874e40f9ca7dec28773425f9a05b408f5e55faba
Nage <Nage>
2023-05-10 11:58:01 +0200

    pide un num

---
+++ pidenum.py
@@ -0,0 +1,10 @@
+def main():
+    msg = "Dame un num: "
+    entrada = ''
+    invalido = False
+    while not entrada.isnumeric():
+        if invalido:
+            print("Obligatoriamente debe ser un número")
+        invalido = True
+        entrada = input(msg)
+    return int(entrada)
